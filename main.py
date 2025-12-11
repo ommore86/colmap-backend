3a0170ec18c44c4e4c2da226f34e94b199c2b0ae
Om More <omgmore2005@gmail.com>
2025-12-11 13:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
+
+class InputModel(BaseModel):
+    email_folder: str
+
+@app.get("/")
+def home():
+    return {"message": "Backend is running 🚀"}
+
+@app.post("/reconstruct")
+def reconstruct(req: InputModel):
+    return {
+        "status": "received",
+        "email_folder": req.email_folder
+    }
